5809c7b720710840007eb6fbb2ee8d81c94e8bf9
Ke Wu <kw2542@columbia.edu>
2014-07-08 17:30:19 -0400

    Solution to Two Sum
    First commit, only contains C++ solution

---
+++ Two Sum/Two Sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        vector<int> res(2);
+        unordered_map<int, int> map(numbers.size());
+        for (int i = 0; i < numbers.size(); ++i) {
+            auto it = map.find(target - numbers[i]);
+            if (it != map.end()) {
+                if (it->second == i + 1) continue;
+                res[0] = it->second;
+                res[1] = i + 1;
+                return res;
+            } else {
+                map[numbers[i]] = i + 1;
+            }
+        }
+      return res;
+    }
+};
