0345bb9c26cc396da00b3590078cd109fe904021
Ke Wu <kw2542@columbia.edu>
2014-07-16 17:21:05 -0400

    Solution to Longest Common Prefix
    Two way: compare char by char or compare string by string

---
+++ Longest Common Prefix/Longest Common Prefix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string> &strs) {
+        if (strs.size() < 1) return "";
+        int index = 0;
+        while (true) {
+            if (index >= strs[0].size()) return strs[0];
+            for (int i = 1; i < strs.size(); ++i)
+                if (strs[i].size() < index + 1 || strs[0][index] != strs[i][index])
+                    if (index > 0) return strs[0].substr(0, index);
+                    else return "";
+            ++index;
+        }
+    }
+};
---
+++ Longest Common Prefix/Longest Common Prefix.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        int index = 0;
+        while (true) {
+            if (index >= strs[0].length()) return strs[0];
+            for (int i = 1; i < strs.length; ++i)
+                if (strs[i].length() < index + 1 || strs[0].charAt(index) != strs[i].charAt(index))
+                    if (index > 0) return strs[0].substring(0, index);
+                    else return "";
+            ++index;
+        }
+    }
+}
