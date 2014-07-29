18bde3f1d328717fc67e3299b8bb85200e7b7e6f
Ke Wu <kw2542@columbia.edu>
2014-07-29 17:19:35 -0400

    Solution to Remove Element

---
+++ Remove Element/Remove Element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(int A[], int n, int elem) {
+        int index = 0;
+        for (int i = 0; i < n; ++i) {
+            if (A[i] != elem) {
+                A[index] = A[i];
+                ++index;
+            }
+        }
+        return index;
+    }
+};
